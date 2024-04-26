08083d24bf1403fa47ba652dfa975d106070ee28
arbab <arbabali0006@gmail.com>
2024-04-26 12:13:08 +0530

    product Images Added to the product page

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -5,13 +5,14 @@ import '../styles/Products.css';
 const Products = () => {
   
   const products = [
-    { id: 1, name: 'Product 1', image: 'url-to-image-1.jpg' },
-    { id: 2, name: 'Product 2', image: 'url-to-image-2.jpg' },
-    { id: 3, name: 'Product 3', image: 'url-to-image-3.jpg' },
-    { id: 4, name: 'Product 4', image: 'url-to-image-4.jpg' },
-    { id: 5, name: 'Product 5', image: 'url-to-image-5.jpg' },
-    { id: 6, name: 'Product 6', image: 'url-to-image-6.jpg' },
-    { id: 7, name: 'Product 7', image: 'url-to-image-7.jpg' },
+    { id: 1, name: 'HP Elitebook 840 G5', image: '/images/product/840g5.jpg'},
+    { id: 2, name: 'Dell Latitude 5400', image: '/images/product/lat5400.jpg'},
+    { id: 3, name: 'HP Elitebook 745 G4', image: '/images/product/745g4.jpg'},
+    { id: 4, name: 'HP Elitebook 820 G4', image: '/images/product/820g4.jpg'},
+    { id: 5, name: 'HP Elitebook 820 G1', image: '/images/product/820g1.jpg'},
+    { id: 6, name: 'HP Elitebook 840 G1', image: '/images/product/840g1.jpg'},
+    { id: 7, name: 'HP Elitebook 8750w Workstation', image: '/images/product/8750w.jpg'},
+    { id: 7, name: 'HP 240 G7', image: '/images/product/240g7.png'}
     
   ];
 
