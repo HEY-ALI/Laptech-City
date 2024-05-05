d49c8e14b1123d6f260eb98fce17117d4f012593
arbab <arbabali0006@gmail.com>
2024-05-05 12:58:30 +0530

    few changes

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -6,78 +6,78 @@ const products = [
       imageUrl: "/images/product/840g5.jpg", // Assuming images are in `public/images/product`
       
       images: [
-        "/images/product/840g5.jpg",
-        "/images/product/840g5.jpg",
+        "/images/product/840g5_01.png",
+        "/images/product/840g5_02.png",
       ], // Additional images
     },
     {
       id: 2,
-      imageUrl: "/images/product/lat5400.jpg",
-      title: "Dell Latitude 5400",
+      imageUrl: "/images/product/820g4.jpg",
+      title: "HP EliteBook 820 G4",
       description: "8gb Ram 256gb SSD",
       images: [
-        "/images/product/lat5400.jpg",
-        "/images/product/lat5400.jpg",
+        "/images/product/820g4_01.png",
+        "/images/product/820g4_02.png",
       ],
     },
     {
         id: 1,
-        imageUrl: "/images/product/840g5.jpg", // Assuming images are in `public/images/product`
-        title: "HP EliteBook 840 G5",
+        imageUrl: "/images/product/lat5400.jpg", // Assuming images are in `public/images/product`
+        title: "Dell latitude 5400",
         description: "8gb RAM  256gb SSD",
         images: [
-          "/images/product/840g5.jpg",
-          "/images/product/840g5.jpg",
+          "/images/product/lat5400_01.jpg",
+          "/images/product/lat5400_02.jpg",
         ], // Additional images
       },
       {
         id: 1,
-        imageUrl: "/images/product/840g5.jpg", // Assuming images are in `public/images/product`
-        title: "HP EliteBook 840 G5",
+        imageUrl: "/images/product/840g1.jpg", // Assuming images are in `public/images/product`
+        title: "HP EliteBook 840 G1",
         description: "8gb RAM  256gb SSD",
         images: [
-          "/images/product/840g5.jpg",
-          "/images/product/840g5.jpg",
+          "/images/product/840g1_01.jpg",
+          "/images/product/840g1_02.jpg",
         ], // Additional images
       },
       {
         id: 1,
-        imageUrl: "/images/product/840g5.jpg", // Assuming images are in `public/images/product`
-        title: "HP EliteBook 840 G5",
+        imageUrl: "/images/product/745g4.jpg", // Assuming images are in `public/images/product`
+        title: "HP EliteBook 745 G4",
         description: "8gb RAM  256gb SSD",
         images: [
-          "/images/product/840g5.jpg",
-          "/images/product/840g5.jpg",
+          "/images/product/745g4_01.jpg",
+          "/images/product/745g4_02.jpg",
         ], // Additional images
       },
       {
         id: 1,
-        imageUrl: "/images/product/840g5.jpg", // Assuming images are in `public/images/product`
-        title: "HP EliteBook 840 G5",
+        imageUrl: "/images/product/8570w.jpg", // Assuming images are in `public/images/product`
+        title: "HP EliteBook 8570w Workstation",
         description: "8gb RAM  256gb SSD",
         images: [
-          "/images/product/840g5.jpg",
-          "/images/product/840g5.jpg",
+          "/images/product/8570w_01.jpg",
+          "/images/product/8570w_02.jpg",
         ], // Additional images
       },
       {
         id: 1,
-        imageUrl: "/images/product/840g5.jpg", // Assuming images are in `public/images/product`
-        title: "HP EliteBook 840 G5",
+        imageUrl: "/images/product/820g1.jpg", // Assuming images are in `public/images/product`
+        title: "HP EliteBook 820 G1",
         description: "8gb RAM  256gb SSD",
         images: [
-          "/images/product/840g5.jpg",
-          "/images/product/840g5.jpg",
+          "/images/product/820g1_01.jpg",
+          "/images/product/820g1_02.jpg",
         ], // Additional images
       },
       {
         id: 1,
-        imageUrl: "/images/product/840g5.jpg", // Assuming images are in `public/images/product`
-        title: "HP EliteBook 840 G5",
+        imageUrl: "/images/product/240g7.png", // Assuming images are in `public/images/product`
+        title: "HP 240 G7",
         description: "8gb RAM  256gb SSD",
         images: [
-          "/images/product/840g5.jpg",
-          "/images/product/840g5.jpg",
+          "/images/product/240g7_01.jpg",
+          "/images/product/240g7_02.jpg",
         ], // Additional images
       },
       {
